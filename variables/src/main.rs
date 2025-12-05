a2ae363df433b6138f7a55f28b7f35ed75799f74
Shubhayan Ghosh <subhayang6@gmail.com>
2025-12-05 23:12:07 +0530

    Add immutable variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,9 @@
+
 fn main() {
-    println!("Hello, world!");
+
+    let /*mut*/ x = 5; // mut is added as comment just to show that keyword makes the variable "x" mutable.
+    println!("The value of x is: {x}");
+
+    x = 6; // This will throw an error as "x" is immutable {https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html}
+    println!("The value of x is {x}");
 }
