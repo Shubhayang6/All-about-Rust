c0d1e1a24e37b9ec6dc57bf61508c3a6f34b81a6
Shubhayan Ghosh <subhayang6@gmail.com>
2025-12-05 23:27:25 +0530

    Add shadowing princple

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,25 @@
-
 fn main() {
-
-    let /*mut*/ x = 5; // mut is added as comment just to show that keyword makes the variable "x" mutable.
+    let mut x = 5; // mut is added as just to show that keyword makes the variable "x" mutable.
     println!("The value of x is: {x}");
 
     x = 6; // This will throw an error as "x" is immutable {https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html}
     println!("The value of x is {x}");
+
+    shadowing();
+}
+
+//declration of constants -> always defined with `type`.
+// const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+fn shadowing() {
+    let x = 5; // actual declaration
+
+    let x = x + 1; // value of x = 5 is shadowed and taken as a new reference
+
+    {
+        let x = x * 2; // same shadowing principle here
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x in the outer scope is: {x}");
 }
